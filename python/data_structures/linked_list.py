ef9869616b5ff8edf3af68e9ab6df0d67b338d82
Adam Owada <adamowada@gmail.com>
2023-04-03 19:44:50 -0700

    code challenge 05

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,49 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    # Insert at the head! aka the beginning of the LinkedList
+    def insert(self, value):
+        old_head = self.head
+        self.head = Node(value)
+        self.head.next = old_head
+
+    def __str__(self):
+        # start with empty string
+        text = ""
+
+        # traverse my linked list
+        current = self.head     # 1) initialize a variable named current, set to head
+
+        while current: # 2) start while loop, choose between current or current.next
+            # 3) Do a thing!
+            text += f"{{ {current.value} }} -> "
+            current = current.next # 4) move the pointer for current
+
+        return text + "NULL"
+
+    def includes(self, value):
+        # traverse my linked list
+        current = self.head  # 1) initialize a variable named current, set to head
+
+        while current:  # 2) start while loop, choose between current or current.next
+            # 3) Do a thing!
+            if current.value == value:
+                return True
+
+            current = current.next  # 4) move the pointer for current
+
+        return False
 
 
 class TargetError:
