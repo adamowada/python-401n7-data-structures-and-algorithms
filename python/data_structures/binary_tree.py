3a64e33bf51b8991b2ad36cc49209f23e25b49a3
Adam Owada <adamowada@gmail.com>
2023-05-11 19:41:53 -0700

    bt with walk helper function

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -3,14 +3,30 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    # root left right
+    def pre_order(self):
+        traversal = []
+
+        def walk(root):
+            if root:
+                # root
+                traversal.append(root.value)  # operation
+                # left
+                walk(root.left)
+                # right
+                walk(root.right)
+
+        # remember to invoke it
+        walk(self.root)
+
+        return traversal
 
 
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
